df067183ad9ade97561d186550b051a61580ef88
mahmoud diab <mahmoud.diab6067796@gmail.com>
2022-08-20 12:29:07 +0200

    test

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+//Madaaaaa
 ####### Login && Register ########
 Route::post('/auth',[AuthController::class,'registerOrLogin']);
 ####### End Login && Register ########
